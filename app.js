82f998721bcd12185d613ea443804e543993f9b5
lohith-dev <srlohith92@gmail.com>
2023-11-15 19:54:40 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors')
+const fs =require('fs');
+const path =require('path');
+const userRouter = require('./routes/userRouter.js');
+const expenseRouter = require('./routes/expenseRouter.js');
+const purchaseRouter = require('./routes/pruchaseRouter.js');
+const leadboardRouter = require('./routes/leadboardRouter.js');
+const passRouter = require('./routes/passwordRouter.js');
 
 const errorController = require('./controllers/error');
 require('dotenv').config();
@@ -11,13 +18,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const userRouter = require('./routes/userRouter.js');
-const expenseRouter = require('./routes/expenseRouter.js');
-const purchaseRouter = require('./routes/pruchaseRouter.js');
-const leadboardRouter = require('./routes/leadboardRouter.js');
-const passRouter = require('./routes/passwordRouter.js');
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname,'access.log'),
+    {flags:'a'}
+);
+
+
+const morgan = require('morgan');
+
 
 app.use(express.json());
+app.use(morgan('combined',{stream:accessLogStream}))
 
 
 app.use('/auth', userRouter);
@@ -29,9 +40,10 @@ app.use('/leadboard', leadboardRouter);
 
 app.use(errorController.get404);
 // {force:true}
+const PORT = process.env.PORT || 3000;
 sequelize.sync().then(result=>{
-    app.listen(8000,()=>{
-        console.log("Server listening at port 8000");
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT} `);
     });
 })
 .catch(err=>{
--- util/database.js
+++ util/database.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize');
+let dotenv = require('dotenv');
 
-const sequelize=new Sequelize('expense','root','root',{
+
+dotenv.config();
+
+const sequelize=new Sequelize( process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD,{
     dialect:'mysql',
-    host : 'localhost'
+    host :process.env.DATABASE_HOST
 });
 
 module.exports = sequelize;
